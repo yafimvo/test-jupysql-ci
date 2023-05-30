c125c91914fa5c3d8fe4347525fb675bf9711315
Mohit Mehta <mehtamohit013@gmail.com>
2023-05-29 20:15:49 -0400

    Lint

--- src/tests/test_magic_plot.py
+++ src/tests/test_magic_plot.py
@@ -236,19 +236,25 @@ def test_bar_one_col(load_data_one_col, ip):
 
 
 @_cleanup_cm()
-@image_comparison(baseline_images=["bar_one_col_h"], extensions=["png"], remove_text=True)
+@image_comparison(
+    baseline_images=["bar_one_col_h"], extensions=["png"], remove_text=True
+)
 def test_bar_one_col_h(load_data_one_col, ip):
     ip.run_cell("%sqlplot bar -t data_one.csv -c x -o h")
 
 
 @_cleanup_cm()
-@image_comparison(baseline_images=["bar_one_col_num_h"], extensions=["png"], remove_text=True)
+@image_comparison(
+    baseline_images=["bar_one_col_num_h"], extensions=["png"], remove_text=True
+)
 def test_bar_one_col_num_h(load_data_one_col, ip):
     ip.run_cell("%sqlplot bar -t data_one.csv -c x -o h -S")
 
 
 @_cleanup_cm()
-@image_comparison(baseline_images=["bar_one_col_num_v"], extensions=["png"], remove_text=True)
+@image_comparison(
+    baseline_images=["bar_one_col_num_v"], extensions=["png"], remove_text=True
+)
 def test_bar_one_col_num_v(load_data_one_col, ip):
     ip.run_cell("%sqlplot bar -t data_one.csv -c x -S")
 
@@ -266,7 +272,9 @@ def test_pie_one_col(load_data_one_col, ip):
 
 
 @_cleanup_cm()
-@image_comparison(baseline_images=["pie_one_col_num"], extensions=["png"], remove_text=True)
+@image_comparison(
+    baseline_images=["pie_one_col_num"], extensions=["png"], remove_text=True
+)
 def test_pie_one_col_num(load_data_one_col, ip):
     ip.run_cell("%sqlplot pie -t data_one.csv -c x -S")
 
